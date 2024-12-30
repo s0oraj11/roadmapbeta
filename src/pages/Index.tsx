dbcccd78a5f70075e642626e1d3c427101777dcb
s0oraj11 <ridesuraj@gmail.com>
2024-12-30 11:37:04 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import React, { Suspense, lazy } from 'react'
 import { motion } from 'framer-motion'
 import { initialNodes, initialEdges } from '@/components/roadmapData'
 
-const StellarRoadmap = lazy(() => import('@/components/stellar-node/index'))
+const StellarRoadmap = lazy(() => import('@/components/stellar-roadmap/index'))
 
 const Index = () => {
   return (
