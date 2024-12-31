ba26d0ecaf7742e57fab49b9003387c2516d5227
s0oraj11 <ridesuraj@gmail.com>
2024-12-31 05:57:19 +0530

    Update index.tsx

--- src/components/stellar-roadmap/index.tsx
+++ src/components/stellar-roadmap/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef } from 'react'
+import React, { useState, useCallback, useRef, useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Stars } from '@react-three/drei'
 import { motion } from 'framer-motion'
